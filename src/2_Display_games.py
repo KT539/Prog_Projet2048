08fb5d3b99199b4ee62ef4594fa30cddc449f288
Kilian Testard <pc47xiy@eduvaud.ch>
2025-01-21 23:11:14 +0100

    feat: added game_in_progress ; display function still wip

--- src/2_Display_games.py
+++ src/2_Display_games.py
@@ -7,16 +7,21 @@ from tkinter import *
 import tkinter.font
 
 # game0 list
-'''game=[[0,0,0,0],
+game0=[[0,0,0,0],
        [0,0,0,0],
        [0,0,0,0],
-       [0,0,0,0]]'''
+       [0,0,0,0]]
 
 # game1 list
-game=[[512,0,2,0],
+"""game1=[[0,0,2,0],
        [0,0,0,0],
        [0,2,0,0],
-       [0,0,0,0]]
+       [0,0,0,0]]"""
+
+game1=[[2048,64,32,16],
+       [1024,16,8,8],
+       [32,512,4,0],
+       [4,0,2,0]]
 
 colors={0:"#CCCCCC",
         2:"#9FC5F8",
@@ -41,9 +46,9 @@ dy=0 # vertical distance between labels
 
 # display game1 values
 def display():
-    for line in range(len(game)):
-        for col in range(len(game[line])):
-            labels[line][col].config(text=game[line][col], bg=colors[game[line][col]])
+    for line in range(len(game1)):
+        for col in range(len(game1[line])):
+            labels[line][col].config(text=game1[line][col], bg=colors[game1[line][col]])
 
 
 
@@ -55,10 +60,10 @@ win.geometry("1000x750")
 Label(text="8192",width=25, height=3,  font=("Arial", 15)).grid(row=0,column=0, columnspan=2,padx=0,pady=0)
 
 #labels creation and position
-for line in range(len(game)):
-    for col in range(len(game[line])):
+for line in range(len(game0)):
+    for col in range(len(game0[line])):
         # creation without placement
-        labels[line][col] = Label (win, text =game[line][col], width=15, height=5, borderwidth=1, relief="solid", font=("Arial", 15), bg="#FFFFFF")
+        labels[line][col] = Label (win, text =game0[line][col], width=15, height=5, borderwidth=1, relief="solid", font=("Arial", 15), bg="#FFFFFF")
         # label positionning in the windows
         labels[line][col].grid (row=line+1,column=col,padx=dx,pady=dy)
 
